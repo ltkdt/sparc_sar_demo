9d895864cf10bbe402a6c42fdfcdda7816c54147
ltkdt <kienlt2306@gmail.com>
2025-10-07 13:37:21 +0700

    folium modification

--- geoview/geoApp/views.py
+++ geoview/geoApp/views.py
@@ -12,80 +12,12 @@ import folium
 import geopandas as gpd
 from folium import GeoJson
 from folium.plugins import MousePosition
+from folium.template import Template
+
 
 # Create your views here.
 def home(request):
     form = LastActiveForm()
-    '''
-    shp_dir = os.path.join(os.getcwd(), 'media', 'shp')
-
-    m = folium.Map(location=[0.0236, 37.9062], zoom_start=6)
-
-    style_Kenya_county_dd = {'fillColor': '#228B22', 'color': '#228B22'}
-    style_kenya_wetlands = {'color': 'blue'}
-    style_kenya_all_towns = {'color': 'red'}
-    style_kenya_highland_roads = {'color': 'yellow'}
-    style_kenya_forestranges = {'color': 'green'}
-
-    Kenya_county_dd = gpd.read_file(os.path.join(shp_dir, 'Kenya_county_dd.shp'))
-    Kenya_county_dd_geojson = Kenya_county_dd.to_crs("EPSG:4326").to_json()
-
-    # to_crs("EPSG:4326")
-
-    kenya_wetlands = gpd.read_file(os.path.join(shp_dir, 'kenya_wetlands.shp'))
-    kenya_wetlands_geojson = kenya_wetlands.to_crs("EPSG:4326").to_json()
-
-    kenya_all_towns = gpd.read_file(os.path.join(shp_dir, 'kenya_all_towns.shp'))
-    kenya_all_towns_geojson = kenya_all_towns.to_crs("EPSG:4326").to_json()
-
-    kenya_highland_roads = gpd.read_file(os.path.join(shp_dir, 'kenya_highland_roads.shp'))
-    kenya_highland_roads_geojson = kenya_highland_roads.to_crs("EPSG:4326").to_json()
-
-    #kenya_forestranges = gpd.read_file(os.path.join(shp_dir, 'kenya_forestranges.shp'))
-    #kenya_forestranges_geojson = kenya_forestranges.to_crs("EPSG:4326").to_json()
-
-    GeoJson(Kenya_county_dd_geojson, name='Kenya_counties', style_function=lambda x: style_Kenya_county_dd).add_to(m)
-    GeoJson(kenya_wetlands_geojson, name='kenya_wetlands', style_function=lambda x: style_kenya_wetlands).add_to(m)
-
-    # Create a feature group for kenya_all_towns layer
-    kenya_all_towns_fg = folium.FeatureGroup(name='kenya_all_towns')
-    # Iterate over the points and add colored circles to the feature group
-    for _, row in kenya_all_towns.iterrows():
-        folium.CircleMarker(location=[row['geometry'].y, row['geometry'].x], radius=2, fill=True, color='red', fill_opacity=1).add_to(kenya_all_towns_fg)
-    kenya_all_towns_fg.add_to(m)
-
-    GeoJson(kenya_highland_roads_geojson, name='kenya_highland_roads', style_function=lambda x: style_kenya_highland_roads).add_to(m)
-    #GeoJson(kenya_forestranges_geojson, name='kenya_forestranges', style_function=lambda x: style_kenya_forestranges).add_to(m)
-
-    folium.LayerControl().add_to(m)
-
-    formatter = "function(num) {return L.Util.formatNum(num, 3) + ' º ';};"
-
-    MousePosition(
-        position="topright",
-        separator=" | ",
-        empty_string="NaN",
-        lng_first=True,
-        num_digits=20,
-        prefix="Coordinates:",
-        lat_formatter=formatter,
-        lng_formatter=formatter,
-    ).add_to(m)
-
-    popup1 = folium.LatLngPopup()
-    m.add_child(popup1)
-    '''
-
-    '''
-    shp_dir = os.path.join(os.getcwd(), 'media', 'dbscl')
-
-    m = folium.Map(location=[9.8999, 106.1556], title='Đồng bằng sông Cửu Long' ,zoom_start=9)
-    style_dbscl = {'fillColor': "#63a6bc", 'color': "#2f81b5"}
-    dbscl = gpd.read_file(os.path.join(shp_dir, 'Songcuulong1.shp'))
-    dbscl_geojson = dbscl.to_crs("EPSG:4326").to_json()
-    GeoJson(dbscl_geojson, name='dbscl', style_function=lambda x: style_dbscl).add_to(m)
-    folium.LayerControl().add_to(m)
-    '''
     shp_dir = os.path.join(os.getcwd(), 'media', 'vietnam')
 
     m = folium.Map(location=[16.4667, 107.5833], title='Viet Nam' ,zoom_start=7)
@@ -132,6 +64,27 @@ def home(request):
 
     popup1 = folium.LatLngPopup()
     m.add_child(popup1)
+
+    popup1._template = Template("""
+            {% macro script(this, kwargs) %}
+                var {{this.get_name()}} = L.popup();
+                function latLngPop(e) {
+                    {{this.get_name()}}
+                        .setLatLng(e.latlng)
+                        .setContent("Latitude: " + e.latlng.lat.toFixed(4) +
+                                    "<br>Longitude: " + e.latlng.lng.toFixed(4))
+                        .openOn({{this._parent.get_name()}});
+                        console.log("Latitude:", e.latlng.lat.toFixed(4));
+                        console.log("Longitude:", e.latlng.lng.toFixed(4));
+
+                        parent.document.getElementById('latInput').value = e.latlng.lat.toFixed(4);
+                        parent.document.getElementById('lngInput').value = e.latlng.lng.toFixed(4);
+                        //parent.document.getElementById('coordForm').submit();
+                        //parent.document.getElementById('mainForm').submit();
+                    }
+                {{this._parent.get_name()}}.on('click', latLngPop);
+            {% endmacro %}
+                                """)
 
     m = m._repr_html_()
     context = {'my_map': m, 'form': form}
